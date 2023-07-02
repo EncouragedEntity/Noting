d6cf0b3823cb96af6548ddd11d36151d564638a4
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-07-02 08:54:31 +0300

    Update firebase_cloud_storage.dart

---
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:noting/services/cloud/cloud_note.dart';
+import 'package:noting/services/cloud/cloud_storage_constants.dart';
+import 'package:noting/services/cloud/cloud_storage_exceptions.dart';
+
+class FirebaseCloudStorage {
+  final notes = FirebaseFirestore.instance.collection("notes");
+
+  Stream<Iterable<CloudNote>> getAllNotes({required String ownerUserId}) =>
+      notes.snapshots().map((event) => event.docs
+          .map((doc) => CloudNote.fromSnapshot(doc))
+          .where((note) => note.ownerUserId == ownerUserId));
+
+  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async {
+    try {
+      return await notes
+          .where(
+            CloudConst.ownerUserIdFieldName,
+            isEqualTo: ownerUserId,
+          )
+          .get()
+          .then((value) => value.docs.map((doc) {
+                return CloudNote(
+                  documentId: doc.id,
+                  ownerUserId:
+                      doc.data()[CloudConst.ownerUserIdFieldName] as String,
+                  content: doc.data()[CloudConst.contentFieldName] as String,
+                );
+              }));
+    } catch (e) {
+      throw CouldNotGetAllNotesException();
+    }
+  }
+
+  void createNewNote({required ownerUserId}) async {
+    await notes.add({
+      CloudConst.ownerUserIdFieldName: ownerUserId,
+      CloudConst.contentFieldName: '',
+    });
+  }
+
+  static final FirebaseCloudStorage _shared =
+      FirebaseCloudStorage._sharedInstance();
+  FirebaseCloudStorage._sharedInstance();
+  factory FirebaseCloudStorage() => _shared;
+}
