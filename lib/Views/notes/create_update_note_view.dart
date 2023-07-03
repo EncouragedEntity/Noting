39d457606eb131c6cc225f887376b8994f64bcc9
Artur <74054908+EncouragedEntity@users.noreply.github.com>
2023-07-03 19:54:31 +0300

    Working sharing

--- lib/Views/notes/create_update_note_view.dart
+++ lib/Views/notes/create_update_note_view.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:noting/services/auth/auth_service.dart';
 import 'package:noting/services/cloud/cloud_note.dart';
 import 'package:noting/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
+
+import '../../utilities/dialogs/cannot_share_empy_note_dialog.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({super.key});
@@ -94,6 +97,22 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
             : _note!.content.isEmpty
                 ? const Text('')
                 : Text(_note!.content.substring(0, 10)),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if(_note==null || text.isEmpty)
+              {
+                await CannotShareEmptyNoteDialog().show(context);
+              }
+              else
+              {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          ),
+        ],
       ),
       body: FutureBuilder(
           future: createOrGetExistingNote(context),
